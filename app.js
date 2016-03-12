396880889414a2ccff152e581e7d6d8c631dbf55
Anton Chupak <anton.chupak@yahoo.com>
2016-03-12 14:48:30 +0200

    first

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+
+        // Optional parameters
+        direction: 'horizontal',
+        autoplay: 5000,
+        speed: 1000,
+        loop: true,
+        parallax: true,
+
+        // Pagination
+        pagination: '.swiper-pagination',
+        paginationClickable: true
+    })
+});
